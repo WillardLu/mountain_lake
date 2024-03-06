83f5b9c5e0c60c221e53a67ecb207b40007ae057
Willard Lu <willard.lu@outlook.com>
2024-03-06 09:48:46 +0800

    basic

---
+++ src/neural_network/neural_network.h
@@ -0,0 +1,26 @@
+// @copyright Copyright 2024 Willard Lu
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+#ifndef NEURAL_NETWORK_NEURAL_NETWORK_H_
+#define NEURAL_NETWORK_NEURAL_NETWORK_H_
+
+#include <iostream>
+#include <unordered_map>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::unordered_map;
+
+// 神经网络类
+class NeuralNetwork {
+ public:
+  NeuralNetwork();
+  ~NeuralNetwork();
+
+ private:
+};
+
+#endif  // NEURAL_NETWORK_NEURAL_NETWORK_H_
